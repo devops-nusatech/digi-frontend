82022cb7b2a8f999c74c4638f45fe064a393e9fe
Muhammad Roisul Adib <99470918+roisuladib@users.noreply.github.com>
2023-03-28 10:45:47 +0700

    css

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
-const USE_SSL = true;
-const BASE_URL = 'api.heavenexchange.io';
+const USE_SSL = false;
+const BASE_URL = '139.180.130.141:3001';
 
 export const API_URL = USE_SSL ? `https://${BASE_URL}` : `http://${BASE_URL}`;
 export const SOCKET_URL = USE_SSL ? `wss://${BASE_URL}` : `ws://${BASE_URL}`;
